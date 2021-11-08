3bc01c7e922245803c555eb718d0f39215b5c697
Richard Wells <rwells1703@gmail.com>
2021-11-08 20:09:50 +0000

    Creates intensity histogram manually and display it via pyplot

--- threshold.py
+++ threshold.py
@@ -1,19 +1,23 @@
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 
-img = cv2.imread("images\\cactus1.jpg", cv2.IMREAD_IGNORE_ORIENTATION)
+bits_per_channel = 8
+bins = 2**bits_per_channel
 
-#cv2.imshow("Cactus", img)
+img = cv2.imread("images\\bubbles.jpg", cv2.IMREAD_GRAYSCALE)
 
-hist = cv2.calcHist([img], [0], None, [256], [0,256])
+#img_disp = cv2.resize(img,(1280,720))
+cv2.imshow("Image", img)
 
-bin = 0
-while bin < 256:
-    print(hist[bin]/img.size)
-    bin += 1
+hist = np.zeros(bins)
 
-#plt.hist(hist)
-#plt.show()
+for x, y in np.ndindex((img.shape[0],img.shape[1])):
+    intensity = img[x,y]
+    hist[intensity] += 1/img.size
+
+plt.plot(range(bins), hist) 
+plt.show()
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
