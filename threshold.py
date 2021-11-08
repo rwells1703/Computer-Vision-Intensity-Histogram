06fba9c0e5794ca2c67b1a7c1d1726803c650460
Richard Wells <rwells1703@gmail.com>
2021-11-08 19:00:52 +0000

    Extracted histogram and normalise it based upon image size

--- threshold.py
+++ threshold.py
@@ -1,8 +1,19 @@
 import cv2
+import matplotlib.pyplot as plt
 
-img = cv2.imread("images\\cactus1.jpg", cv2.IMREAD_COLOR)
+img = cv2.imread("images\\cactus1.jpg", cv2.IMREAD_IGNORE_ORIENTATION)
 
-cv2.imshow("Cactus", img)
+#cv2.imshow("Cactus", img)
+
+hist = cv2.calcHist([img], [0], None, [256], [0,256])
+
+bin = 0
+while bin < 256:
+    print(hist[bin]/img.size)
+    bin += 1
+
+#plt.hist(hist)
+#plt.show()
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
